f0b80147b9ba8f00d2dedb618ff2ddbf6e01433a
Lina Fadhli <133816965+luluna02@users.noreply.github.com>
2024-11-05 18:13:54 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -72,6 +72,16 @@ int main(int argc, char *argv[]) {
                             printf("EQUAL = null\n");
                         }
                         break;
+                    case '!':
+                        // check if anoother = exists?
+                        if(file_contents[i+1] == '='){
+                            printf("BANG_EQUAL != null\n");
+                            i++;
+                        }
+                        else{
+                            printf("BANG ! null\n");
+                        }
+                        break;
                     default:
                         fprintf(stderr,"[line %d] Error: Unexpected character: %c\n", 1, file_contents[i]);
                         EXIT_CODE = 65;
