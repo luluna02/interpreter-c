1b2f8586d22417e5a116dad097554174f81bb464
Lina Fadhli <133816965+luluna02@users.noreply.github.com>
2024-12-27 19:34:24 +0100

    codecrafters submit [skip ci]

--- src/evaluate.c
+++ src/evaluate.c
@@ -16,6 +16,13 @@ EvalResult evaluate_expr(Expr* expr) {
             } else if (value.type == NIL) {
                 result.is_nil = true;
             }
+            else if (value.type == NUMBER) {
+                result.is_number = true;
+                result.number_value = *((double*)value.literal);;
+            } else if (value.type == STRING) {
+                result.is_string = true;
+                result.string_value = strdup((char*)value.literal); // Copy string
+            }
             break;
         }
         case GROUPING: {
@@ -30,11 +37,15 @@ EvalResult evaluate_expr(Expr* expr) {
     return result;
 }
 
-void print_eval_result(EvalResult result) {
+void print_eval_result(EvalResult result){
     if (result.is_nil) {
         printf("nil\n");
     } else if (result.is_boolean) {
         printf("%s\n", result.boolean_value ? "true" : "false");
+    } else if (result.is_number) {
+        printf("%.10g\n", result.number_value);
+    } else if (result.is_string) {
+        printf("%s\n", result.string_value);
     } else {
         fprintf(stderr, "Unexpected evaluation result\n");
     }
--- src/evaluate.h
+++ src/evaluate.h
@@ -6,9 +6,13 @@
 
 // Define the result of evaluation
 typedef struct {
-    bool is_nil;         // Represents if the value is nil
-    bool is_boolean;     // Represents if the value is a boolean
-    bool boolean_value;  // Holds the boolean value
+    bool is_nil;          // Represents if the value is nil
+    bool is_boolean;      // Represents if the value is a boolean
+    bool boolean_value;   // Holds the boolean value
+    bool is_number;       // Represents if the value is a number
+    double number_value;  // Holds the number value
+    bool is_string;       // Represents if the value is a string
+    char *string_value;   // Holds the string value (null-terminated)
 } EvalResult;
 
 // Evaluate an expression
